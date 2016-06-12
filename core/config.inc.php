063ef5cd4d8efbad345c1c01314b584503968b0a
Horsell <ihorsell@protonmail.ch>
2016-06-12 21:37:11 +0200

    First commit file

---
+++ core/config.inc.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+* DATABASE
+*/
+define('DB_TYPE', 'mysql');
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'collectify');
+define('DB_USER', 'horsell');
+define('DB_PASSWORD', 'Integra0@@');
+
+/**
+* Redbean configuration
+*/
+define('REDBEAN_MODEL_PREFIX', '\\collectify\\model\\');
